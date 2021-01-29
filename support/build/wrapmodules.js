0a51e9c95d1a59023852ccc1feb0cc468ba4ff84
mrcork <stucork@gmail.com>
2021-01-29 19:58:29 +0800

    use closure to wrap stdlib

--- support/build/wrapmodules.js
+++ support/build/wrapmodules.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const minify = require('babel-minify');
+const { compiler: Compiler } = require("google-closure-compiler");
 
 /**
  * If this optional file exists in the top level directory, it will be
@@ -21,88 +21,153 @@ const minify = require('babel-minify');
  * distribution.
  */
 const excludeFileName = "libexcludes.json";
+let js_bytes = 0;
 
-function processDirectories(dirs, recursive, exts, ret, minifyjs, excludes) {
-    dirs.forEach((dir) => {
+async function processDirectories(dirs, exts, ret, options) {
+    const {production, languageOut, excludes, recursive} = options;
+    for (let dir of dirs) {
         let files = fs.readdirSync(dir);
 
-        files.forEach((file) => {
+        for (let file of files) {
             let fullname = dir + '/' + file;
 
             if (!excludes.includes(fullname)) {
-                let stat = fs.statSync(fullname)
+                let stat = fs.statSync(fullname);
 
                 if (recursive && stat.isDirectory()) {
-                    processDirectories([fullname], recursive, exts, ret, minifyjs, excludes);
+                    await processDirectories([fullname], exts, ret, options);
                 } else if (stat.isFile()) {
                     let ext = path.extname(file);
                     if (exts.includes(ext)) {
-                        let contents = fs.readFileSync(fullname, 'utf8');
-                        if (minifyjs && (ext == ".js")) {
-                            let result = minify(contents, {
-                                mangle: {
-                                    keepFnName: true,
-                                },
-                                deadcode: {
-                                    keepFnName: true,
-                                },
+                        if (production && (ext === ".js")) {
+                            console.log(`Compiling ${fullname}...`);
+
+                            // STANDARD
+                            opts = {
+                              language_in: 'ECMASCRIPT_NEXT',
+                              language_out: languageOut,
+                              source_map_include_content: true,
+                            };
+
+                            Object.assign(opts, {
+                                js: fullname,
+                                jscomp_error: [
+                                    "accessControls",
+                                    "checkRegExp",
+                                    "checkVars" /*'checkTypes',*/,
+                                    "invalidCasts",
+                                    "missingProperties",
+                                    "nonStandardJsDocs",
+                                    "strictModuleDepCheck",
+                                    "undefinedVars",
+                                    "unknownDefines",
+                                    "visibility",
+                                ],
+                                jscomp_off: [
+                                    "fileoverviewTags",
+                                    "deprecated",
+                                    "uselessCode",
+                                    "suspiciousCode",
+                                    "checkTypes",
+                                ],
+                                languageOut,
+                                externs: "support/externs/sk.js",
+
+                                // https://stackoverflow.com/questions/43807412/shared-polyfills-in-google-closure-compiler#43835873
+                                rewritePolyfills: false,
+                                // injectLibraries: false, // This will prevent use of async/await. Removing this will allow async/await, but will include all the necessary polyfills at the top of every file that needs them.
+
+                                // useful for debugging
+                                // warningLevel: "QUIET",
+                                // formatting: 'PRETTY_PRINT',
+                                // debug: true,
+                                // renaming: false
                             });
-                            contents = result.code;
+
+                            const compiler = new Compiler(opts);
+
+                            let contents = await new Promise((resolve, reject) => {
+                                compiler.run((exitCode, stdOut, stdErr) => {
+                                    if (exitCode === 0) {
+                                        resolve(stdOut);
+                                    } else {
+                                        reject(stdErr);
+                                    }
+                                });
+                            });
+                            const kb = Math.round(Buffer.byteLength(contents, "utf8")/1000);
+                            console.log(`${kb} kb`);
+                            js_bytes += kb;
+
+                            ret.files[fullname] = contents;
+
+                        } else {
+                            ret.files[fullname] = fs.readFileSync(fullname, "utf8");
                         }
-                        ret.files[fullname] = contents;
                     }
                 }
             }
-        });
-    });
+        }
+    }
 };
 
 
-function buildJsonFile(name, dirs, exts, outfile, options) {
+async function buildJsonFile(name, dirs, exts, outfile, options) {
     options = options || {};
-    let recursive = options.recursive || false;
-    let minifyjs = options.minifyjs || false;
-    let excludes = options.excludes || [];
-    let dir, file;
-    let ret = {};
+    const ret = {files: {}};
 
-    ret.files = {};
+    await processDirectories(dirs, exts, ret, options);
 
-    processDirectories(dirs, recursive, exts, ret, minifyjs, excludes);
-
-    let contents = "Sk." + name + "=" + JSON.stringify(ret);
-    fs.writeFileSync(outfile, contents, 'utf8');
-    console.log("Updated " + outfile + ".");
+    const contents = "Sk." + name + "=" + JSON.stringify(ret, null, 2);
+    fs.writeFileSync(outfile, contents, "utf8");
+    console.log(`js lib size: ${js_bytes} kb`);
+    console.log("\nUpdated " + outfile + ".");
 }
 
-if (process.argv.includes("internal")) {
-    // buildJsonFile("internalPy", ["src"], [".py"], "src/internalpython.js");
-} else if (process.argv.includes("builtin")) {
-    let excludes = [];
-    if (fs.existsSync(excludeFileName)) {
-        excludes = JSON.parse(fs.readFileSync(excludeFileName));
-    }
-    let opts = {
-        recursive: true,
-        minifyjs: true,
-        excludes: excludes
-    };
+async function main() {
+    if (process.argv.includes("stdlib")) {
+        let excludes = [];
+        if (fs.existsSync(excludeFileName)) {
+            excludes = JSON.parse(fs.readFileSync(excludeFileName));
+        }
 
-    buildJsonFile("builtinFiles", ["src/builtin", "src/lib"], [".js", ".py"], "dist/skulpt-stdlib.js", opts)
-    if (process.argv.includes("prod")) {
-        updateConstructorNames();
+        const production = process.argv.includes("prod");
+        const langMatch = process.env.npm_config_argv.match(/env\.languageOut=(?<lang>\w+)/);
+        const languageOut = (langMatch && langMatch.groups.lang) || "ECMASCRIPT_2015";
+        console.log(languageOut);
+ 
+
+        const opts = {
+            recursive: true,
+            excludes: excludes,
+            production,
+            languageOut
+       };
+
+        await buildJsonFile("builtinFiles", ["src/builtin", "src/lib"], [".js", ".py"], "dist/skulpt-stdlib.js", opts);
+        let stat = fs.statSync("dist/skulpt-stdlib.js");
+        
+        console.log(`\nstd-lib size: ${Math.round(stat.size/1000)} kb`);
+        if (production) {
+            updateConstructorNames();
+        }
+    } else if (process.argv.includes("unit2")) {
+        if (!fs.existsSync("support/tmp")) {
+        fs.mkdirSync("support/tmp");
+        }
+        buildJsonFile("unit2", ["test/unit"], [".py"], "support/tmp/unit2.js", { recursive: true });
+    } else if (process.argv.includes("unit3")) {
+        if (!fs.existsSync("support/tmp")) {
+        fs.mkdirSync("support/tmp");
+        }
+        buildJsonFile("unit3", ["test/unit3"], [".py"], "support/tmp/unit3.js");
     }
-} else if (process.argv.includes("unit2")) {
-    if (!fs.existsSync("support/tmp")) {
-	fs.mkdirSync("support/tmp");
-    }
-    buildJsonFile("unit2", ["test/unit"], [".py"], "support/tmp/unit2.js", { recursive: true });
-} else if (process.argv.includes("unit3")) {
-    if (!fs.existsSync("support/tmp")) {
-	fs.mkdirSync("support/tmp");
-    }
-    buildJsonFile("unit3", ["test/unit3"], [".py"], "support/tmp/unit3.js");
+
 }
+
+main().catch(e => {
+    console.error(e);
+});
 
 
 /**
@@ -113,18 +178,22 @@ if (process.argv.includes("internal")) {
  */
 
 function updateConstructorNames() {
-    const minFile = "dist/skulpt.min.js";
-    fs.readFile(minFile, "utf8", (err, data) => {
-        if (err) {
-            return console.log(err);
-        }
-        const result = data.replace(
-            /\.([\w]+)=Sk\.abstr\.build(Native|Iterator)Class\("([\w]+)",\{constructor:function\(/g,
-            '.$1=Sk.abstr.build$2Class("$3",{constructor:function $1('
-        );
+    try {
+        const minFile = "dist/skulpt.min.js";
+        fs.readFile(minFile, "utf8", (err, data) => {
+            if (err) {
+                return console.error(err);
+            }
+            const result = data.replace(
+                /\.([\w]+)=Sk\.abstr\.build(Native|Iterator)Class\("([\w]+)",\{constructor:function\(/g,
+                '.$1=Sk.abstr.build$2Class("$3",{constructor:function $1('
+            );
 
-        fs.writeFile(minFile, result, "utf8", function (err) {
-            if (err) return console.log(err);
+            fs.writeFile(minFile, result, "utf8", function (err) {
+                if (err) return console.log(err);
+            });
         });
-    });
+    } catch (e) {
+        console.error(e);
+    }
 }
