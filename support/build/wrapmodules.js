5b92516a1795d6d6152bd36496f621e3a9db53ab
Stu Cork <stu@anvil.works>
2021-06-10 06:14:46 +0000

    delete string.py cherrypick keyword.js

--- support/build/wrapmodules.js
+++ support/build/wrapmodules.js
@@ -112,7 +112,7 @@ async function processDirectories(dirs, exts, ret, options) {
 };
 
 const fastFilePaths = new Set(["src/builtin/sys.js", "src/lib/time.js", "src/lib/datetime.py", "src/lib/math.js"]);
-const medFilePaths = new Set([, "src/lib/itertools.js", "src/lib/functools.js", "src/lib/random.js", "src/lib/collections.js", "src/lib/operator.js", "src/lib/keyword.py", "src/lib/string.js", "src/lib/re.js"]);
+const medFilePaths = new Set([, "src/lib/itertools.js", "src/lib/functools.js", "src/lib/random.js", "src/lib/collections.js", "src/lib/operator.js", "src/lib/keyword.js", "src/lib/string.js", "src/lib/re.js"]);
 
 function loadSkulptFastSlow(ret, name, outfile) {
     const retFiles = ret.files;
